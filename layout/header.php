29060df5f231470831ff11d94993f6a87ee00371
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-11-29 12:05:25 -0300

    Creación modelo mvc proyecto Inatrans

--- layout/header.php
+++ layout/header.php
@@ -6,25 +6,25 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<title>Intranet Inatrans</title>
 
-	<link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
-	<link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
-	<link href="img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
-	<link href="img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
-	<link href="img/favicon.png" rel="icon" type="image/png">
-	<link href="img/favicon.ico" rel="shortcut icon">
+	<link href="assets/img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
+	<link href="assets/img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
+	<link href="assets/img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
+	<link href="assets/img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
+	<link href="assets/img/favicon.png" rel="icon" type="image/png">
+	<link href="assets/img/favicon.ico" rel="shortcut icon">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-	<link rel="stylesheet" href="css/lib/lobipanel/lobipanel.min.css">
-	<link rel="stylesheet" href="css/separate/vendor/lobipanel.min.css">
-	<link rel="stylesheet" href="css/lib/jqueryui/jquery-ui.min.css">
-	<link rel="stylesheet" href="css/separate/pages/widgets.min.css">
-    <link rel="stylesheet" href="css/lib/font-awesome/font-awesome.min.css">
-    <link rel="stylesheet" href="css/lib/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="assets/css/lib/lobipanel/lobipanel.min.css">
+	<link rel="stylesheet" href="assets/css/separate/vendor/lobipanel.min.css">
+	<link rel="stylesheet" href="assets/css/lib/jqueryui/jquery-ui.min.css">
+	<link rel="stylesheet" href="assets/css/separate/pages/widgets.min.css">
+    <link rel="stylesheet" href="assets/css/lib/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/lib/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body class="with-side-menu control-panel control-panel-compact">
 
@@ -32,7 +32,7 @@
 	    <div class="container-fluid">
 	
 	        <a href="#" class="site-logo">
-	            <img class="hidden-md-down" src="img/logo.jpg" alt="">
+	            <img class="hidden-md-down" src="assets/img/logo.jpg" alt="">
 	        </a>
 	
 	        <button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
@@ -49,7 +49,7 @@
 						<!-- div Icono del perfil -->
 	                    <div class="dropdown user-menu">
 	                        <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	                            <img src="img/avatar-2-64.png" alt="">
+	                            <img src="assets/img/avatar-2-64.png" alt="">
 	                        </button>
 	                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
 	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-user"></span>Perfil</a>
