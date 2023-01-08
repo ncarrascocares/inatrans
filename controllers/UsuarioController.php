45061f5e74d9f9cb0896210c632caee351a3002e
Nicolas Carrasco <nicolascarrasco15@gmail.com>
2023-01-08 01:03:41 -0300

    Se crea la variable de sesion id

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -22,10 +22,12 @@ require_once 'models/Usuario.php';
 
                     if ($correo == $resul->Correo && $password == $resul->password) {
                         session_start();
+                        $_SESSION['id'] = $resul->id;
                         $_SESSION['nombre'] = $resul->Nombre;
                         $_SESSION['apellido'] = $resul->Apellido;
-                        $_SESSION['tipo_user'] = $resul->usuario_nombre;
+                        $_SESSION['tipo_user'] = $resul->nombre;
 
+                      
                         header("Location:".base_url."index.php");
                     }else{
                         header("Location:".base_url."login.php");
