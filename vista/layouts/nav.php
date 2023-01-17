8ef23d0d99a4c42926e03465e2ee3160f2e51b49
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-01-17 18:21:35 -0300

    se agrega opción Gestion Usuarios

--- vista/layouts/nav.php
+++ vista/layouts/nav.php
@@ -74,6 +74,14 @@
                                 </p>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a href="../vista/adm_usuario.php" class="nav-link">
+                                <i class="nav-icon fas fa-user-cog"></i>
+                                <p>
+                                    Gestion Usuarios
+                                </p>
+                            </a>
+                        </li>
                         <li class="nav-header">EXAMPLES</li>
 
                         <li class="nav-item">
