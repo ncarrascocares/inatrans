a991a184cee83ec111b739299797d6744e29e687
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-01-24 16:48:51 -0300

    Función para la inserción de los datos en la bd

--- modelo/Odt.php
+++ modelo/Odt.php
@@ -13,15 +13,31 @@ class Odt{
     }
 
     function listar_reportes(){
-        $sql = "SELECT re.id_interno, re.Simulador_id, re.Averia_reporte, re.Comentario_reporte, re.Fecha_crea, concat(us.Nombre_us,' ',us.Apellido_us) as Responsable, ca.Nombre_categoria, av.Nombre_averia
+        $sql = "SELECT re.id_reporte, re.Simulador_id, re.Averia_reporte, re.Comentario_reporte, re.Fecha_crea, concat(us.Nombre_us,' ',us.Apellido_us) as Responsable, ca.Nombre_categoria, av.Nombre_averia
         FROM reporte re 
         inner join usuario us on re.Usuario_id = us.id_usuario
         inner join categoria ca on re.Categoria_id = ca.id_categoria
         inner join averia av on re.Tipo_averia_id = av.id_averia
-        WHERE RE.Estatus_reporte != 0;";
+        WHERE re.Estatus_reporte != 0;";
         $query = $this->acceso->prepare($sql);
         $query->execute();
         $this->objetos=$query->fetchAll();
         return $this->objetos;
+    }
+
+    function guardar_reportes($id_usuario,$simulador_id,$instructor,$averia_reporte,$comentario_reporte,$categoria_id,$fecha_crea,$tipo_averia_id){
+        $sql = "INSERT INTO reporte (Simulador_id, Usuario_id, Instructor, Averia_reporte, Comentario_reporte, Categoria_id, Fecha_crea, Tipo_averia_id) 
+                            VALUES (:simulador,:id_usuario, :instructor, :averia_reporte, :comentario,:categoria, :fecha, :tipo)";
+        $query = $this->acceso->prepare($sql);
+        $query->execute(array(  ':simulador' =>$simulador_id,
+                                ':id_usuario'=>$id_usuario,
+                                ':instructor'=>$instructor,
+                                ':averia_reporte'=>$averia_reporte,
+                                ':comentario'=>$comentario_reporte,
+                                ':categoria'=>$categoria_id,
+                                ':fecha'=>$fecha_crea,
+                                ':tipo'=>$tipo_averia_id));
+        echo "odt-insertada";
+    
     }
 }
