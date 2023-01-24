679175d97d2c10f8ee59be1b588f035a0b134737
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-01-24 16:46:26 -0300

    Se modifica el front de la tabla y se crea la acción para el guardado de nuevos reportes

--- js/Odt.js
+++ js/Odt.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-    let funcion = 'listar_reportes';
 
+    let id_usuario = $('#id_usuario').val();
+
+    let funcion = 'listar_reportes';
     $('#tabla_reporte').DataTable({
         ajax: {
             "url": "../controlador/OdtController.php",
@@ -8,7 +10,7 @@ $(document).ready(function() {
             "data": { funcion: funcion }
         },
         columns: [
-            { data: 'id_interno' },
+            { data: 'id_reporte' },
             { data: 'simulador_id' },
             { data: 'averia_reporte' },
             { data: 'comentario_reporte' },
@@ -16,12 +18,11 @@ $(document).ready(function() {
             { data: 'responsable' },
             { data: 'nombre_categoria' },
             { data: 'nombre_averia' },
-            { "defaultContent": `<button class="btn btn-success"><i class="fas fa-search"></i></button>
-                                <button class="btn btn-danger"><i class="fas fa-window-close"></i></button>` }
+            { "defaultContent": `<button class="btn btn-success" style="font-size:50%"><i class="fas fa-search"></i></button>
+                                    <button class="btn btn-danger"style="font-size:50%"><i class="fas fa-window-close"></i></button>` }
         ],
         "language": espanol
     });
-
 
     //Listando los valores del select para seleccionar el simulador
     let select = document.getElementById("simulador_id");
@@ -31,6 +32,43 @@ $(document).ready(function() {
         option.text = "Simulador " + i;
         select.appendChild(option);
     }
+
+    $('#form-crear-reporte').submit(e => {
+        let reporte = [];
+        //Recibiendo los datos del formulario
+        let id_usuario = $('#id_usuario').val();
+        let simulador_id = $('#simulador_id').val();
+        let instructor = $('#instructor').val();
+        let averia_reporte = $('#averia_reporte').val();
+        let comentario_reporte = $('#comentario_reporte').val();
+        let categoria_id = $('#categoria_id').val();
+        let fecha_crea = $('#fecha_crea').val();
+        let tipo_averia_id = $('#tipo_averia_id').val();
+
+        let funcion = "guardar_reporte";
+
+        $.post('../controlador/OdtController.php', { id_usuario, simulador_id, instructor, averia_reporte, comentario_reporte, categoria_id, fecha_crea, tipo_averia_id, funcion }, (response) => {
+            console.log(response);
+            if (response == 'odt-insertada') {
+                $('#new-report').hide('slow');
+                $('#new-report').show(1000);
+                $('#new-report').hide(2000);
+
+                //Accion para que todos los campos input se reseteen
+                $('#form-crear-reporte').trigger('reset');
+                buscar_reportes();
+            } else {
+                $('#no-new-report').hide('slow');
+                $('#no-new-report').show(1000);
+                $('#no-new-report').hide(2000);
+
+                //Accion para que todos los campos input se reseteen
+                $('#form-crear-reporte').trigger('reset');
+            }
+        });
+
+        e.preventDefault();
+    })
 
 });
 
