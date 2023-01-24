6c10a515b0211cdf5c2e60beb80c38ef4a654f5d
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-01-24 16:47:55 -0300

    Función para enviar los datos hacia el modelo para su posterior insert

--- controlador/OdtController.php
+++ controlador/OdtController.php
@@ -17,3 +17,18 @@ if($_POST['funcion'] == 'listar_reportes'){
     $jsonstring = json_encode($json);
     echo $jsonstring;
 }
+
+if($_POST['funcion'] == 'guardar_reporte'){
+    $id_usuario = $_POST['id_usuario'];
+    $simulador_id = $_POST['simulador_id'];
+    $instructor = ucfirst($_POST['instructor']);
+    $averia_reporte = $_POST['averia_reporte'];
+    $comentario_reporte = $_POST['comentario_reporte'];
+    $categoria_id = $_POST['categoria_id'];
+    $fecha_crea = $_POST['fecha_crea'];
+    $tipo_averia_id = $_POST['tipo_averia_id'];
+
+    $reporte->guardar_reportes($id_usuario,$simulador_id,$instructor,$averia_reporte,$comentario_reporte,$categoria_id,$fecha_crea,$tipo_averia_id);
+     //die();
+    
+}
