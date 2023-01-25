257021f308302df7d16ef7bdb8498b9370fb1939
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-01-25 18:09:21 -0300

    js para los detalles de las odt

---
+++ js/Odt_detalles.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    let id_usuario = $('#id_usuario').val();
+    let id_reporte = $('#id_reporte').val();
+
+    console.log(id_usuario);
+    console.log(id_reporte);
+
+
+})
