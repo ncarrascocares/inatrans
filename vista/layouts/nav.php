6db17f92d0e93757daf909b994a73874a2b1bdf4
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-02-20 18:46:08 -0300

    Modificación del front para opción de equipos

--- vista/layouts/nav.php
+++ vista/layouts/nav.php
@@ -83,6 +83,15 @@
                                 </p>
                             </a>
                         </li>
+                        <li class="nav-header">Mantenimiento</li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fa fa-cogs"></i>
+                                <p>
+                                    Equipos
+                                </p>
+                            </a>
+                        </li>
                     </ul>
                     <?php endif; ?>
                     </ul>
