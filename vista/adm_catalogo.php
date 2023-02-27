b6806d06c716b99d3e90f86bc0fd2d581a0618a8
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-02-27 16:40:45 -0300

    corrección en el front

--- vista/adm_catalogo.php
+++ vista/adm_catalogo.php
@@ -24,7 +24,7 @@ if (!empty($_SESSION['usuario_tipo'])) {
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item">Home</li>
                             <li class="breadcrumb-item active">Resumen</li>
                         </ol>
                     </div>
@@ -61,7 +61,7 @@ if (!empty($_SESSION['usuario_tipo'])) {
                                 <div class="icon">
 
                                 </div>
-                                <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+                                <span class="small-box-footer"><i class="fas fa-ticket-alt"></i></span>
                             </div>
                         </div>
                         <!-- ./col -->
