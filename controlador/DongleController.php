1e7ecc5d160327bd17f3c688aef0c5ccdacfc4a2
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2023-03-14 12:59:22 -0300

    codificación del backend para la inserción y listado de información de la bd

--- controlador/DongleController.php
+++ controlador/DongleController.php
@@ -5,13 +5,24 @@ session_start();
 
 $dongle = new Dongle();
 
-switch ($variable) {
-    case 'value':
-        # code...
+switch ($_POST['funcion']) {
+    case 'listar_dongle':
+        $json = array();
+        $dongle->listar_dongle();
+        foreach ($dongle->objetos as $objeto) {
+            $json[] = array(
+            'id_dongle'=>$objeto->id_dongle,
+            'identificador'=>$objeto->identificador,
+            'fec_ven'=>$objeto->fec_ven
+            );
+        }
+        $jsonstring = json_encode($json);
+        echo $jsonstring;
         break;
-    
-    default:
-        # code...
+    case 'new_dongle':
+        $num_serial = strtoupper($_POST['num_serial']);
+        $fecha_ven = $_POST['fecha_ven'];
+        $dongle->new_dongle($num_serial,$fecha_ven);
         break;
 }
 
--- modelo/Dongle.php
+++ modelo/Dongle.php
@@ -12,6 +12,28 @@ class Dongle{
         $this->acceso = $db->pdo;
     }
     
+    function listar_dongle(){
+        $sql = "SELECT * FROM dongle";
+        $query = $this->acceso->prepare($sql);
+        $query->execute();
+        $this->objetos=$query->fetchAll();
+        return $this->objetos;
+    }
+
+    function new_dongle($num_serial,$fecha_ven){
+        $sql = "SELECT * FROM dongle WHERE identificador = :num_serial;";
+        $query = $this->acceso->prepare($sql);
+        $query->execute(array(':num_serial'=>$num_serial));
+        $this->objetos=$query->fetchAll();
+        if(!empty($this->objetos)){
+            echo 'dongle-existe';
+        }else{
+            $sql = "INSERT INTO dongle(identificador, fec_ven) VALUES (:num_serial,:fec_ven)";
+            $query = $this->acceso->prepare($sql);
+            $query->execute(array(':num_serial'=>$num_serial,'fec_ven'=>$fecha_ven));
+            echo 'dongle-add';
+        }
+    }
 }
 
 
