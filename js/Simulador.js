6598a64dc8979f1130c8694c0d12bc25ea3827a4
ncarrascocares <nicolascarrasco15@gmail.com>
2025-10-20 23:37:45 -0300

    Se agrega la acción para el formulario del modal de los nuevos sub equipos

--- js/Simulador.js
+++ js/Simulador.js
@@ -23,12 +23,48 @@ $(document).ready(function() {
             const manten = JSON.parse(response);
             let template = '<option>Selecciona mantención</option>';
             manten.forEach(manten => {
-                template += `<option value="${manten.id_mantenimiento}">${manten.nombre_mantenimiento}</option>`;
+                template += `<option value="${manten.id_manten}">${manten.nom_manten}</option>`;
             });
 
             tipo_mant.innerHTML = template;
         });
     }
+
+
+    // Este es el submit del formulario para agregar un nuevo sub-equipo
+    // Se activa al presionar el btn crear del tipo submit del modal
+     $('#form_new_sub').submit(e => {
+
+        funcion = 'nuevo_sub';
+        let name = $('#name_equipo').val();
+        let sim = $('#selectsimulador').val();
+        let manten = $('#tipo_mant').val();
+        let detalle = $('#detalle_sub').val();
+
+        // Validación de los campos del formulario
+        if (name == '' || sim == '' || manten == '' || detalle == '') {
+            $('#no-datos-sub').hide('slow');
+            $('#no-datos-sub').show(1000);
+            $('#no-datos-sub').hide(2000);
+        } else {
+            // Aquí va la llamada al controlador para enviar los datos al modelo y la base de datos
+            $.post('../controlador/SubEquipoController.php', { name, sim, manten, detalle, funcion }, (response) => {
+                if (response == 'add') {
+                    $('#add-sub').hide('slow');
+                    $('#add-sub').show(1000);
+                    $('#add-sub').hide(2000);
+                    $('#form_new_sub').trigger('reset');
+                }   else {
+                    $('#no-add-sub').hide('slow');
+                    $('#no-add-sub').show(1000);
+                    $('#no-add-sub').hide(2000);
+                    $('#form_new_sub').trigger('reset');
+                }
+            });
+        } 
+
+        e.preventDefault();
+    });
 
     $('#new_element').submit(e => {
         let name = $('#name_equipo').val();
@@ -40,9 +76,8 @@ $(document).ready(function() {
         
         console.log(name,periocidad,codigo,sub,tipo,detalles);
 
+        e.preventDefault();
      });
-
-     
 
 })
 
