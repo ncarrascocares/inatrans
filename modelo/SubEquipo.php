101aaa0abfa8fc8f4baeb35e387098d702f2e33b
ncarrascocares <nicolascarrasco15@gmail.com>
2025-10-23 00:19:57 -0300

    Correcciones a las funciones para la vañidaciones de nuevos sub equipos, además se corrige el select para poder llenar la tabla del front

--- modelo/SubEquipo.php
+++ modelo/SubEquipo.php
@@ -15,7 +15,10 @@ class SubEquipo{
     }
 
     function listar(){
-        $sql = "SELECT * FROM sub_equipo where 1";
+        $sql = "select sub.nombre_sub,sub.detalle_sub,si.Nombre_simulador,ma.nombre_mantenimiento,ma.descripcion_mantenimiento from sub_equipo sub
+                left join simulador si on sub.simulador_id = si.id_simulador
+                left join mantenimiento ma on sub.mantenimiento_id = ma.id_mantenimiento
+                where 1;";
         $query = $this->acceso->prepare($sql);
         $query->execute();
         $this->objetos=$query->fetchAll();
@@ -23,11 +26,22 @@ class SubEquipo{
     }
 
     // Función para crear un nuevo sub-equipo
+    // En el caso de haber un sub equipo con el mismo nombre para el mismo simulador, no se debe agregar
     function crear($nombre,$detail,$id_sim,$id_man){
-        $sql = "INSERT INTO sub_equipo (nombre_sub, detalle_sub,simulador_id,mantenimiento_id) VALUES (:nombre, :detalle, :id_sim, :id_man)";
-        $query = $this->acceso->prepare($sql);
-        $query->execute(array(':nombre'=>$nombre, ':detalle'=>$detail, ':id_sim'=>$id_sim, ':id_man'=>$id_man));
-        echo "add";
+        $select = "SELECT id_sub_equipo  FROM sub_equipo WHERE nombre_sub=:nombre AND simulador_id=:id_sim";
+        $query = $this->acceso->prepare($select);
+        $query->execute(array(':nombre'=>$nombre, ':id_sim'=>$id_sim));
+        $this->objetos = $query->fetchAll();
+        // Este if valida si ya existe un sub equipo con el mismo nombre para el mismo simulador
+        if(!empty($this->objetos)){
+            echo 'noadd';
+            return;
+        }else{ // Si no existe, se procede a insertar el nuevo sub equipo
+            $insert = "INSERT INTO sub_equipo (nombre_sub, detalle_sub,simulador_id,mantenimiento_id) VALUES (:nombre, :detalle, :id_sim, :id_man)";
+            $query = $this->acceso->prepare($insert);
+            $query->execute(array(':nombre'=>$nombre, ':detalle'=>$detail, ':id_sim'=>$id_sim, ':id_man'=>$id_man));
+            echo "add";
+        }
     }
 
 }
